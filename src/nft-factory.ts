a98d3fa7da63d2a3a6b4af4f14e553a66149c2e1
milo <1@qq.com>
2024-07-21 21:55:13 +0800

    thegraph

--- src/nft-factory.ts
+++ src/nft-factory.ts
@@ -1,3 +1,4 @@
+import { BigInt,log  } from "@graphprotocol/graph-ts";
 import {
   NFTCreated as NFTCreatedEvent,
   NFTRegesitered as NFTRegesiteredEvent,
@@ -6,20 +7,54 @@ import {
 import {
   NFTCreated,
   NFTRegesitered,
-  OwnershipTransferred
+  OwnershipTransferred,
+  TokenInfo
 } from "../generated/schema"
+import { S2NFT } from "../generated/NFTFactory/S2NFT"
 
 export function handleNFTCreated(event: NFTCreatedEvent): void {
   let entity = new NFTCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+  let id = event.params.nftCA.toHex();
+  let tokenInfo = new TokenInfo(id);
   entity.nftCA = event.params.nftCA
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
-  entity.save()
+  let nftContract = S2NFT.bind(event.params.nftCA);
+
+  // Try to get baseURI
+  let baseURICall = nftContract.try__baseURI();
+  if (!baseURICall.reverted) {
+    tokenInfo.tokenURL = baseURICall.value;
+    log.info("Base URI: {}", [baseURICall.value]);
+  } else {
+    tokenInfo.tokenURL = "";
+    log.warning("Base URI call reverted for contract: {}", [event.params.nftCA.toHex()]);
+  }
+
+  // Try to get name
+  let nameCall = nftContract.try_name();
+  if (!nameCall.reverted) {
+    tokenInfo.name = nameCall.value;
+    log.info("NFT Name: {}", [nameCall.value]);
+  } else {
+    tokenInfo.name = "";
+    log.warning("Name call reverted for contract: {}", [event.params.nftCA.toHex()]);
+  }
+
+  tokenInfo.ca = event.params.nftCA;
+  tokenInfo.tokenId = BigInt.fromI32(0); // Initial tokenId, actual value should be set when token is created
+  tokenInfo.owner = event.params.nftCA; // Set to contract address initially, update later
+  tokenInfo.blockNumber = event.block.number;
+  tokenInfo.blockTimestamp = event.block.timestamp;
+  tokenInfo.transactionHash = event.transaction.hash;
+
+  entity.save();
+  tokenInfo.save();
 }
 
 export function handleNFTRegesitered(event: NFTRegesiteredEvent): void {
@@ -31,8 +66,20 @@ export function handleNFTRegesitered(event: NFTRegesiteredEvent): void {
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
+  let id = event.params.nftCA.toHex();
+  let tokenInfo = TokenInfo.load(id);
 
-  entity.save()
+  if (tokenInfo == null) {
+    tokenInfo = new TokenInfo(id);
+  }
+
+  // 更新 tokenInfo 的 blockNumber 和 blockTimestamp
+  tokenInfo.blockNumber = event.block.number;
+  tokenInfo.blockTimestamp = event.block.timestamp;
+  tokenInfo.transactionHash = event.transaction.hash;
+
+  entity.save();
+  tokenInfo.save();
 }
 
 export function handleOwnershipTransferred(
@@ -48,5 +95,12 @@ export function handleOwnershipTransferred(
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
-  entity.save()
+  let id = event.params.newOwner.toHex();
+  let tokenInfo = TokenInfo.load(id);
+  if (tokenInfo != null) {
+    tokenInfo.owner = event.params.newOwner;
+    tokenInfo.save();
+  }
+
+  entity.save();
 }
